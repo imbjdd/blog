68452703d9f90ec054e6d43db3f95e5144eeffed
imbjdd <b0bb1e@protonmail.com>
2025-02-10 19:14:44 +0100

    feat: remove name

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <section className="flex-col flex justify-center mt-8 mb-4 md:my-16 md:mb-12">
       <Link href="/" className="tracking-tighter leading-tight md:pr-8 font-bold">
-        Salim Boujaddi
+        Salim Bjdd
       </Link>
       <p>GitHub</p>
       <p>LinkedIn</p>
